a3299b2a2caa4d179a2881a230bd970a5c66a541
Bastien Aumeunier <bastien.aumeunier@ynov.com>
2022-10-11 16:04:19 +0200

    New folder

---
+++ WIK-DPS-TP01/TP/src/App.ts
@@ -0,0 +1,24 @@
+import * as dotenv from "dotenv"
+import express from 'express'
+
+const app = express();
+
+const port = process.env.PING_LISTEN_PORT == null ? 8080 : process.env.PING_LISTEN_PORT
+
+dotenv.config();
+
+app.listen(port, () => {
+    console.log('Lancement du server sur le port : ' + port);
+});
+
+
+app.get('/ping', (req, res)=> {
+    res.json(JSON.stringify(req.headers)).status(200)
+})
+
+app.get('*', (_, res)=> {
+    res.status(404)
+    res.send(' ')
+});
+
+
